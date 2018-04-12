70e6767c5036121ebb8e85f7c524727926136bc0
Kouui <lelouchkako@gmail.com>
2018-04-12 22:28:46 +0900

    timeout for shower

---
+++ calibration/MMSP2Lib.py
@@ -0,0 +1,17 @@
+"""
+author: kouui
+date: 2018/04/12
+
+This is a library related to MMSP(2) issues.
+MMSP : Mueller Matrix Spectro-Polarimetry
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def showMuellerMatrixVsWavelength(mm, wl):
+    
+    
+    pass
